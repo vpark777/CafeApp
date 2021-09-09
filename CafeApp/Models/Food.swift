dace969437e201457a1b512251e0a8a9f67b8999
helloMayuko <mayuko@hellomayuko.com>
2021-09-09 15:14:55 -0700

    Setting up starter project with Menu data models

---
+++ CafeApp/Models/Food.swift
@@ -0,0 +1,22 @@
+//
+//  Food.swift
+//  CafeApp
+//
+//  Created by Mayuko Inoue on 9/9/21.
+//
+
+import Foundation
+
+struct Food: MenuItem {
+    var imageName: String
+    var name: String
+    var description: String
+    var price: Double
+    
+    init(withImageName imageName: String, withName name: String, withDescription description: String, withPrice price: Double) {
+        self.imageName = imageName
+        self.name = name
+        self.description = description
+        self.price = price
+    }
+}
