c4406fb7815ca1f6c636962511e9c5db5b1d7b40
helloMayuko <mayuko@hellomayuko.com>
2021-09-09 16:35:04 -0700

    Building the tableview, the cell and headerview layouts

--- CafeApp/Views/MenuViewController.swift
+++ CafeApp/Views/MenuViewController.swift
@@ -16,6 +16,16 @@ class MenuViewController: UIViewController {
         return headerView
     }()
     
+    var menuTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = UIColor(named: "Cream")
+        tableView.layer.cornerRadius = 20
+        tableView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(MenuItemTableViewCell.self, forCellReuseIdentifier: "DefaultItemCell")
+        tableView.register(MenuTableHeaderView.self, forHeaderFooterViewReuseIdentifier: "MenuHeaderView")
+        return tableView
+    }()
     
 // MARK: - Initializers
 
@@ -36,7 +46,13 @@ class MenuViewController: UIViewController {
 // MARK: - UI Setup Functions
     
      func setupUI() {
+        view.backgroundColor = .white
+        
+        menuTableView.dataSource = self
+        menuTableView.delegate = self
+
         view.addSubview(menuHeaderView)
+        view.addSubview(menuTableView)
     }
     
      func activateConstraints() {
@@ -44,6 +60,11 @@ class MenuViewController: UIViewController {
             menuHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             menuHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 26),
             menuHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -26),
+            
+            menuTableView.topAnchor.constraint(equalTo: menuHeaderView.bottomAnchor, constant: 24),
+            menuTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            menuTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            menuTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
     
@@ -52,5 +73,40 @@ class MenuViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+}
+
+extension MenuViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DefaultItemCell") as! MenuItemTableViewCell
+        cell.imageName = "drinks_coffee"
+        cell.itemName = "Drip Coffee"
+        cell.itemDescription = "Our daily house drip coffee"
+        cell.itemPrice = 2.0
+        return cell
+    }
+}
+
+extension MenuViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = tableView.dequeueReusableHeaderFooterView(withIdentifier: "MenuHeaderView") as! MenuTableHeaderView
+        if(section == 0) {
+            cell.headerName = "Drinks"
+        } else if(section == 1) {
+            cell.headerName = "Food"
+        } else if(section == 2) {
+            cell.headerName = "Merch • Other"
+        } else {
+            cell.headerName = "Other"
+        }
+        return cell
     }
 }
